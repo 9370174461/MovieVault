ff7a6fbe603d0320ad29978619a1fd301f91f48f
Mahesh Ubale <maheshubale9244@gmail.com>
2023-12-31 20:33:45 +0530

    commit 4

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 /* styles/globals.css */
 import "bootstrap/dist/css/bootstrap.min.css";
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,7 @@ export default function RootLayout({ children }) {
           <Navbar />
         </div>
         {children}
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+        {/* <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script> */}
       </body>
     </html>
   );
