e07ee13ea95bc373654abaf34c97d764b716af1a
Mahesh Ubale <maheshubale9244@gmail.com>
2024-01-01 10:24:38 +0530

    UI Optimization 1

--- src/app/topmovie/[movieid]/page.js
+++ src/app/topmovie/[movieid]/page.js
@@ -1,8 +1,6 @@
 // movieid.js
 "use client";
 
-
-
 import { useEffect, useState } from "react";
 import { BiSolidStar } from "react-icons/bi";
 import { FaThumbsUp } from "react-icons/fa6";
@@ -26,27 +24,49 @@ export default function Movieid({ params }) {
     return () => {};
   }, []);
 
-  
   if (!movie) {
     return <div>Movie not found</div>;
   }
 
   return (
-    <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg my-3 p-3"  style={{color:"white"}}>
-      <h1 className="d-flex align-items-center justify-content-center my-3 p-3" > {movie.title}</h1>
+    <div
+      className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg my-3 p-3"
+      style={{ color: "white" }}
+    >
+      <h1 className="d-flex align-items-center justify-content-center my-3 p-3">
+        {movie.title}
+      </h1>
+      <div className="col-lg-3 align-items-center justify-content-center offset-lg-1 p-0 overflow-hidden shadow-lg">
+        <img
+          className="rounded-lg-3"
+          style={{ width: "98%" }}
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt=""
+          width="720"
+        />
+      </div>
       <div className="col-lg-7 p-3 p-lg-5 pt-lg-3">
-
         <h1 className="display-4 fw-bold lh-1 text-body-emphasis"></h1>
         <h3>Overview </h3>
         <p className="lead"> {movie.overview}</p>
         <div className="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
-          <button type="button" className="btn btn-primary btn-lg px-4 me-md-2 fw-bold"><BiSolidStar />{movie.vote_average}</button>
-          <button type="button" className="btn btn-outline-secondary btn-lg px-4"> {movie.vote_count}  <FaThumbsUp size={25} color="white"/></button>
-          <button type="button" className="btn btn-outline-light">Realiase On {movie.release_date}</button>
+          <button
+            type="button"
+            className="btn btn-primary btn-lg px-4 me-md-2 fw-bold"
+          >
+            <BiSolidStar />
+            {movie.vote_average}
+          </button>
+          <button
+            type="button"
+            className="btn btn-outline-secondary btn-lg px-4"
+          >
+            {movie.vote_count} <FaThumbsUp size={25} color="white" />
+          </button>
+          <button type="button" className="btn btn-outline-light">
+            Realiase On {movie.release_date}
+          </button>
         </div>
-      </div>
-      <div className="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
-          <img className="rounded-lg-3"style={{width:"80%"}} src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" width="720"/>
       </div>
     </div>
   );
