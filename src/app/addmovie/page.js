b10018e61889d9c88c9a7e4bea6af7b4a564fc3e
Mahesh Ubale <maheshubale9244@gmail.com>
2024-01-19 22:01:40 +0530

    addmovie page

---
+++ src/app/addmovie/page.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function addpage() {
+  return (
+    <div>
+      <div className="mb-3">
+  <label for="exampleFormControlInput1" className="form-label">Email address</label>
+  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+</div>
+<div className="mb-3">
+  <label for="exampleFormControlInput1" className="form-label">Email address</label>
+  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+</div>
+<div className="mb-3">
+  <label for="exampleFormControlInput1" className="form-label">Email address</label>
+  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+</div>
+<div className="mb-3">
+  <label for="exampleFormControlInput1" className="form-label">Email address</label>
+  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+</div>
+    </div>
+  )
+}
