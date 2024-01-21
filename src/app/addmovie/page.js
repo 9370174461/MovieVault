2aa41caa1c3d961e25c86ac0c0bb8b679f1e69a8
Mahesh Ubale <maheshubale9244@gmail.com>
2024-01-21 18:21:32 +0530

    upload

--- src/app/addmovie/page.js
+++ src/app/addmovie/page.js
@@ -3,21 +3,21 @@ import React from 'react'
 export default function addpage() {
   return (
     <form>
-      <div className='container mx-auto p-2'>
+      <div className='container '>
   <div className="mb-3">
     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
     <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+    <div id="emailHelp" className="form-text">Well never share your email with anyone else.</div>
   </div>
   <div className="mb-3">
     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
     <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+    <div id="emailHelp" className="form-text">Well never share your email with anyone else.</div>
   </div>
   <div className="mb-3">
     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
     <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+    <div id="emailHelp" className="form-text">Well never share your email with anyone else.</div>
   </div></div>
 
     </form>
